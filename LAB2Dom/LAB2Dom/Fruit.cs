b6a8a5ca07d98047713a22346d8c2f962bd25b87
Mieszko Parys <31597120+Miechox@users.noreply.github.com>
2022-03-25 14:08:25 +0100

    Lab 2 zad dom

---
+++ LAB2Dom/LAB2Dom/Fruit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2Dom
+{
+    internal class Fruit : Product
+    {
+        public int Count { get { return _count; } set { _count = value; } }
+        private int _count;
+
+        public Fruit(string name, int count) : base(name)
+        {
+            this._count = count;
+        }
+
+        public override void Print(string prefix)
+        {
+            string[] temp = { "fruit", "fruits" };
+            Console.WriteLine(prefix + $"{this.Name} ({this._count} {(this._count>1?temp[1]:temp[2])})");
+        }
+    }
+}
---
+++ LAB2Dom/LAB2Dom/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB2Dom
+{
+    internal class Product : IThing
+    {
+        public string Name { get { return _name; } set { _name = value; } }
+        private string _name;
+
+        public Product(string name)
+        {
+            _name = name;
+        }
+        public virtual void Print(string prefix)
+        {
+            Console.WriteLine("Product");
+        }
+    }
+}
